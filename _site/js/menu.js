ef9d23cc8b3ec8f91be14543d9aa0e409c7fd619
Leon Bentrup <leon@leonadi.de>
2015-12-01 22:10:21 +0100

    Remove _site from git tree

--- _site/js/menu.js
+++
@@ -1,9 +0,0 @@
-//document.addEventListener("ready", function () {
-  var toggle = document.getElementById("nav-toggle");
-  var navbar = document.getElementById("nav-top");
-  var open = false;
-  toggle.addEventListener("click", function () {
-    open = !open;
-    navbar.className = open ? "navbar open" : "navbar";
-  });
-//});
